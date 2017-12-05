5fb06ff07371ba6852202c1a1fd30b88f97cf57d
nate4495 <njm4dd@virginia.edu>
2017-12-04 23:17:00 -0500

    Update project.py
    Complete algorithm

--- project.py
+++ project.py
@@ -7,14 +7,26 @@ F = ["F","B", "C", "D", "E"];
 graph = [A,B,C,D,E,F];
 cutset = [];
 
-def remove(v,g):
-    g.remove(v)
+def remove(v,g): #helper function that removes the node from main graph and other lists that contain it
+    g.remove(v) #remove the node from graph
     print(str(v[0]));
     for x in g:
         for i in x:
-            if i == str(v[0]):
+            if i == str(v[0]):  #remove any other isntances of the node
                 x.remove(i) 
 
 for x in graph:
-    if len(x) < 3:
-        remove(x, graph)  
+    if len(x) < 3: #if degree is 1 or 0, remove from graph
+        remove(x, graph)
+while len(graph) != 0: #while graph is not empty
+    maxDegree = 0 # init for maxDegree value
+    maxNode = graph[0] #init for maxNode refrence
+    for node in graph: 
+        if len(node) > maxDegree:  #find highest degree node and store it
+            maxDegree = len(node)
+            maxNode = node
+    cutset.append(maxNode) #add this node to the cutset
+    remove(maxNode,graph) #remove all refrences from original graph
+    for x in graph:
+        if len(x) < 3: #if degree is 1 or 0, remove from graph
+            remove(x, graph)
