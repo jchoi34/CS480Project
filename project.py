e93ede865ce715f6084c02dbee9500beef5d12a5
nate4495 <njm4dd@virginia.edu>
2017-12-04 23:42:57 -0500

    Update project.py
    removed print

--- project.py
+++ project.py
@@ -7,9 +7,8 @@ F = ["F","B", "C", "D", "E"];
 graph = [A,B,C,D,E,F];
 cutset = [];
 
-def remove(v,g): #helper function that removes the node from main graph and other lists that contain it
+def remove(v,g):
     g.remove(v) #remove the node from graph
-    print(str(v[0]));
     for x in g:
         for i in x:
             if i == str(v[0]):  #remove any other isntances of the node
@@ -18,15 +17,15 @@ def remove(v,g): #helper function that removes the node from main graph and othe
 for x in graph:
     if len(x) < 3: #if degree is 1 or 0, remove from graph
         remove(x, graph)
-while len(graph) != 0: #while graph is not empty
-    maxDegree = 0 # init for maxDegree value
-    maxNode = graph[0] #init for maxNode refrence
-    for node in graph: 
-        if len(node) > maxDegree:  #find highest degree node and store it
+while len(graph) != 0:
+    maxDegree = 0 # init for max
+    maxNode = graph[0] 
+    for node in graph:
+        if len(node) > maxDegree:
             maxDegree = len(node)
             maxNode = node
-    cutset.append(maxNode) #add this node to the cutset
-    remove(maxNode,graph) #remove all refrences from original graph
+    cutset.append(maxNode)
+    remove(maxNode,graph)
     for x in graph:
         if len(x) < 3: #if degree is 1 or 0, remove from graph
             remove(x, graph)
